961b5b5339f4e68524da7148b1ee69c1c5e9dd4f
Lee Edwards <leeredwards@gmail.com>
2026-01-20 02:30:10 -0800

    Remove .next from tracking

--- .next/dev/static/chunks/app_layout_1cf6b850.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_112093ed._.js",
-  "static/chunks/src_534865ef._.css"
-],
-    source: "dynamic"
-});
